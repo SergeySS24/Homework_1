7f538f401e88228c50742ba8e2082518cbd7e558
Sergey Starostin <adelaide.star@gmail.com>
2022-05-31 20:31:13 +0300

    added some first tests

--- src/test/java/com/SS1/Registration_Form.java
+++ src/test/java/com/SS1/Registration_Form.java
@@ -1,10 +1,34 @@
 package com.SS1;
 
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+
 public class Registration_Form {
 
+    @BeforeAll
+    static void setUp() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1020";
+    }
+
+    @Test
+    void FillFormTest() {
+
+        open("/automation-practice-form");
+
+        $("[id=firstName]").setValue("Sergey");
+        $("[id=lastName]").setValue("Starostin");
+        $("[id=userEmail]").setValue("sergey@example.com");
+
+    }
 
 
 
 
-    
+
 }
