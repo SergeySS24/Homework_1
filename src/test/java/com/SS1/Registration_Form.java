7aed9f009934a4dc675b5c3d243996d4a5107de1
Sergey Starostin <adelaide.star@gmail.com>
2022-06-01 19:35:04 +0300

    added some calendar actions

--- src/test/java/com/SS1/Registration_Form.java
+++ src/test/java/com/SS1/Registration_Form.java
@@ -1,12 +1,14 @@
 package com.SS1;
 
 import com.codeborne.selenide.ClickMethod;
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.commands.Click;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -37,10 +39,13 @@ public class Registration_Form {
         //Mobile
         $("#userNumber").setValue("89111234567");
 
+        //Date of Birth
+        $("[id=dateOfBirthInput]").shouldHave(value("01 Jun 2022"));
+        $("[id=dateOfBirthInput]").click();
+        $("[class=react-datepicker__month-select]").click();
+        
 
 
 
     }
-
-
 }
