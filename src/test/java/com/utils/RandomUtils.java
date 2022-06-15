6a5a01288a418e5e111ef3ee07cecef4cf817ebd
Sergey Starostin <adelaide.star@gmail.com>
2022-06-15 23:48:04 +0300

    Added first random method

---
+++ src/test/java/com/utils/RandomUtils.java
@@ -0,0 +1,16 @@
+package com.utils;
+
+import java.util.Random;
+
+public class RandomUtils {
+    public static String getRandomString(int length) {
+        String SALTCHARS = "ABCDEFGHIJKLMNOPQSTUVWXYZ1234567890";
+        StringBuilder result = new StringBuilder();
+        Random rnd = new Random();
+        while (result.length() < length) {
+            int index = (int) (rnd.nextFloat() * SALTCHARS.length());
+            result.append(SALTCHARS.charAt(index));
+        }
+        return result.toString();
+    }
+}
