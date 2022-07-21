98bfb79463e54ec2e467b4677e69a8c139eea3c6
Sergey Starostin <adelaide.star@gmail.com>
2022-07-22 00:16:01 +0300

    Added steps2

--- src/test/java/com/PageObjects_myCode/Steps1.java
+++ src/test/java/com/PageObjects_myCode/Steps1.java
@@ -1,6 +1,8 @@
 package com.PageObjects_myCode;
 
+import com.codeborne.selenide.logevents.SelenideLogger;
 import com.pages.PageObjects.training.StepSupport;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.Test;
 
 public class Steps1 {
@@ -9,7 +11,13 @@ public class Steps1 {
 
     @Test
     public void initiateTests() {
-        step.prepareTest();
-        step.setFirstName("Sergey");
+        SelenideLogger.addListener("star", new AllureSelenide());
+        step.prepareTest()
+                .setFirstName("Sergey")
+                .setLastName("Starostin")
+                .setEmail("adelaide.star@sss.com")
+                .setGender("Male")
+                .setPhone("89992525543");
+
     }
 }
--- src/test/java/com/pages/PageObjects/training/StepSupport.java
+++ src/test/java/com/pages/PageObjects/training/StepSupport.java
@@ -1,19 +1,47 @@
 package com.pages.PageObjects.training;
 
 import io.qameta.allure.Step;
+import org.openqa.selenium.By;
 
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class StepSupport {
 
     @Step("open webpage")
-    public void prepareTest() {
+    public StepSupport prepareTest() {
         open("https://demoqa.com/automation-practice-form");
+        return this;
     }
 
     @Step("set first name")
-    public void setFirstName(String name) {
-        $("#firstName").setValue(name);
+    public StepSupport setFirstName(String name) {
+        $("#firstNames").setValue(name);
+        return this;
+    }
+
+    @Step("set last name")
+    public StepSupport setLastName(String surname) {
+        $("#lastName").setValue(surname);
+        return this;
+    }
+
+    @Step("set email")
+    public StepSupport setEmail(String mail) {
+        $("#userEmail").setValue(mail);
+        return this;
+    }
+
+    @Step("set gender")
+    public StepSupport setGender(String gender) {
+        $(byText(gender)).click();
+        return this;
+    }
+
+    @Step("set phone number")
+    public StepSupport setPhone(String number) {
+        $(By.id("userNumber")).setValue(number);
+        return this;
     }
 }
